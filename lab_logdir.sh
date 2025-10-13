97e692c6bac2198e1a5183445f353fae66d444eb
Garlinn <matveika108@gmail.com>
2025-10-13 15:19:29 +0300

    Changed format of input and output

--- lab_logdir.sh
+++ lab_logdir.sh
@@ -1,7 +1,11 @@
-echo "Enter /log directory path: "
-read log_dir
-echo "Enter Enter threshold(N): "
-read N
+if [ $# -lt 2 ]; then
+	echo "Usage: $0 <log_directory_path> <size>"
+	exit 1
+fi
+
+log_dir="$1"
+size="$2"
+N=70
 
 if [ -z "$log_dir" ]; then
     echo "Path is empty"
@@ -18,25 +22,14 @@ if [ ! -d "$log_dir" ]; then
     exit 4
 fi
 
-if ! [[ "$N" =~ ^[0-9]+$ ]]; then
-        echo "Error: threshold must be an integer"
-        exit 1
-fi
+dir_size=$(du -sb "$log_dir" | awk '{print $1}')
+perc=$(( dir_size * 100 / size))
+threshold=$(( N * 100 / size))
 
-dir_size=$(du -s "$log_dir" | awk '{print $1}')
+echo "Directory $log_dir takes $perc% of the given size."
 
-filesys_size=$(df "$log_dir" | tail -1 | awk '{print $2}')
-
-perc=$(( dir_size * 100 / filesys_size ))
-
-echo "Directory $log_dir takes $perc% of the filesystem."
-
-if [ "$perc" -ge "$N" ]; then
-	echo "The usage exceeds the threshold($N%). Starting archivation.."
-	init_time=$(date +%Y%m%d_%H%M%S)
-	archive="${log_dir%/}_backup_$init_time.tar.gz"
-	tar -czf "$archive" -C "$(dirname "$log_dir")" "$(basename "$log_dir")"
-	echo "Directory $log_dir archived into $archive"
+if [ "$dir_size" -ge "$threshold" ]; then
+	echo "The usage exceeds the threshold($threshold). Archivation needed."
 else
-	echo "The usage is less then threshold($N%). No archivation needed."
+	echo "The usage is less then threshold($threshold). No archivation needed."
 fi
